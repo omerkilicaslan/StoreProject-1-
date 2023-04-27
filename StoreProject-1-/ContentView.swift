944b390981eb6ef1f590321a5fdb3d527b7fc442
omerkilicaslan <omerrr_2001@hotmail.com>
2023-04-27 20:41:14 +0300

    Initial Commit

---
+++ StoreProject-1-/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoreProject-1-
+//
+//  Created by Ömer Faruk Kılıçaslan on 27.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoreProject-1-/StoreProject_1_App.swift
@@ -0,0 +1,17 @@
+//
+//  StoreProject_1_App.swift
+//  StoreProject-1-
+//
+//  Created by Ömer Faruk Kılıçaslan on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreProject_1_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
